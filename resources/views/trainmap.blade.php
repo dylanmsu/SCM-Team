1ec5435b9307d8bd594b9e78c3bbd91719067821
Tijl Desmet <desmet.tijl@gmail.com>
2020-10-20 23:11:43 +0200

    Your limitation, it's only your imagination

--- resources/views/trainmap.blade.php
+++ resources/views/trainmap.blade.php
@@ -24,11 +24,11 @@
            * https://codepen.io/aniketkudale/pen/BaNxomQ
            * Dutch palette
            */
-          array('img' => './images/map/svg/hlv.svg',        'name' => 'Stoomlocomotief',  'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=hlv&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
-          array('img' => './images/map/svg/mw.svg',         'name' => 'Motorwagen',       'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=ttmw&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
-          array('img' => './images/map/svg/hlr.svg',        'name' => 'Rangeerloc',       'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=hlr&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
-          array('img' => './images/map/svg/sms.svg',        'name' => 'Smalspoor',        'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=sms&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
-          array('img' => './images/map/svg/plus.svg',       'name' => 'Extra Locomotief', 'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=plus&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
+          array('img' => './images/map/hlv.svg',        'name' => 'Stoomlocomotief',  'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=hlv&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
+          array('img' => './images/map/mw.svg',         'name' => 'Motorwagen',       'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=ttmw&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
+          array('img' => './images/map/hlr.svg',        'name' => 'Rangeerloc',       'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=hlr&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
+          array('img' => './images/map/sms.svg',        'name' => 'Smalspoor',        'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=sms&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
+          array('img' => './images/map/plus.svg',       'name' => 'Extra Locomotief', 'link' => "https://track.scm-team.be/Resources/ScriptPlugins/com.GpsGate/Publish/v3/single_gmap.html?appId=6&viewId=7&userName=plus&mapKey=AIzaSyAQLRtDypeBHfYsRSwbQnoTUY0euHEy7aQ&units=metric&v=5.0.0.4781"),
           
       );
   @endphp
@@ -51,7 +51,7 @@
                   @auth
                   <div class="my-3 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" >
                      <a class="card my-card text-decoration-none" href="https://track.scm-team.be" target="_blank">
-                         <img class="card-img-top" src="./images/map/svg/tracking.svg">
+                         <img class="card-img-top" src="./images/map/tracking.svg">
                          <div class="py-3 card-footer text-uppercase"><strong>Tracking Console</strong></div>
                      </a>
                  </div>
