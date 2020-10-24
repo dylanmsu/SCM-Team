a96ad3bf5134c7840d8787a2003e214a87565362
Dylan Missuwe <dylan.missu@gmail.com>
2020-10-24 15:39:27 +0200

    updated deploy command

--- app/Console/Commands/DeployCommand.php
+++ app/Console/Commands/DeployCommand.php
@@ -38,7 +38,45 @@ class DeployCommand extends Command
     public function handle()
     {
         echo "deploying laravel... \n";
-        echo shell_exec('deploy.sh');
+        # Turn on maintenance mode
+        shell_exec('php artisan down || true');
+
+        # Pull the latest changes from the git repository
+        # git reset --hard
+        # git clean -df
+        echo "pulling changes from github...";
+        shell_exec('git pull origin master');
+
+        # Install/update composer dependecies
+        shell_exec('composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev');
+
+        # Run database migrations
+        shell_exec('php artisan migrate');
+
+        # Clear caches
+        shell_exec('php artisan cache:clear');
+
+        # Clear expired password reset tokens
+        shell_exec('php artisan auth:clear-resets');
+
+        # Clear and cache routes
+        shell_exec('php artisan route:cache');
+
+        # Clear and cache config
+        shell_exec('php artisan config:cache');
+
+        # Clear and cache views
+        shell_exec('php artisan view:cache');
+
+        # Install node modules
+        # npm ci
+
+        # Build assets using Laravel Mix
+        shell_exec('npm run production');
+
+        # Turn off maintenance mode
+        shell_exec('php artisan up');
+
         return 0;
     }
 }
