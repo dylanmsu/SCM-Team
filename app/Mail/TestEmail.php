c42303a60000154398bec71d44bd181bd948f18f
Dylan Missuwe <dylan.missu@gmail.com>
2021-01-24 17:12:48 +0100

    added sequrity.md

---
+++ app/Mail/TestEmail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+        $address = 'info@scm-team.be';
+        $subject = 'Dit is een test!';
+        $name = 'SCM-Team';
+
+        return $this->view('emails.test')
+                    ->from($address, $name)
+                    ->cc($address, $name)
+                    ->bcc($address, $name)
+                    ->replyTo($address, $name)
+                    ->subject($subject)
+                    ->with([ 'test_message' => $this->data['message'] ]);
+    }
+}
